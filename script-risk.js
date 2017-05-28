8a4cf1c9969ec8cf07ea22ac0bb5bb685d37995d
sergey <ssyreyshchikov@mail.ru>
2017-05-28 15:00:16 +0300

    stage3

---
+++ script-risk.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+	
+	// открытие модального окна
+	$(".progressbar_risk li").on("click", function(){
+		var $this = $(this);
+		var $modal = $("#modal");
+		var $trigger = false;
+		// Ищем элементы с классом фолс, чтобы запретить дальнейшую обработку
+		$(".progressbar_risk li").each(function(){
+			// Если наiли, то ставим триггер в положение фолс
+			if($(this).hasClass("fail")){
+				$trigger = true;
+			}
+		});
+		// Проверяем завершенность предыдущего этапа
+		if($(this).prev().hasClass("progresel_risk") && 
+			!$(this).prev().hasClass("complete") ||
+			$(this).prev().hasClass("preparing")){
+			return false;
+		}
+		if(!$(this).next().hasClass("progresel_risk")){
+			return false;	
+		}
+		// Проверяем завершенность текущего этапа
+		if($(this).hasClass("complete") || $trigger) 
+			return false;
+		// Открываем модальное окно
+		if($modal.hasClass("hide")){
+			showHideModal($modal, "hide", "show", $this.attr("id"));
+		}else{
+			showHideModal($modal, "show", "hide", "");
+		}
+	});
+
+	// Обработка действий модали
+	$("#variants li").on("click", function(){
+		$modal  = $("#modal");
+		// Соответствует идентификатору прогрессбара
+		$hidVal = $modal.find('input[type="hidden"]').val();
+		$attr   = $(this).attr("varius");
+		eventMy($hidVal, $attr);
+
+		showHideModal($modal, "show", "hide", "");
+	})
+
+});
+
+var eventMy = function(step, action){
+	var $thisStep = $("#"+step);
+	var $category = $("."+step);
+	var $class;
+	switch(action){
+		case "1":
+			$classSt  = "active complete";
+			$classCat = "circle-color-lime";
+			break;
+		case "2":
+			$classSt  = "preparing";
+			$classCat = "circle-risk-color-orange";
+			break;
+		case "3":
+			$classSt  = "fail";
+			$classCat = "circle-risk-color-red";
+			break;
+		default:
+			console.log("Неправильный тип");
+			break;			
+	}
+	$thisStep.attr("class", $classSt);
+	$category.find(".circle").addClass($classCat);
+}
+// Функция открытия модального окна
+var showHideModal = function(context, classNameRenove, classNameAdd, hideValue){
+	context.removeClass(classNameRenove);
+	context.addClass(classNameAdd);
+	context.find('input[type="hidden"]').val(hideValue);
+}
